e3ffff0f3e2de44bed716ff124c049abafee1350
awebkit <32680341@qq.com>
2012-09-27 22:39:50 +0800

    0926 version, new demo

---
+++ AutoNaviTBTDemo0926/AutoNaviTBTDemo/src/com/autonavi/tbt/demo/AutoNaviTBTDemoActivity.java
@@ -0,0 +1,30 @@
+package com.autonavi.tbt.demo;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AutoNaviTBTDemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starting);
+        
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+         @Override
+	         public void run() {
+	        	 Intent intent = new Intent(AutoNaviTBTDemoActivity.this, TBTNaviWarningActivity.class); 
+	        	 startActivity(intent);
+	        	 AutoNaviTBTDemoActivity.this.finish();
+	         }
+         
+        };
+    
+        timer.schedule(task, 2000);		
+    }
+}
